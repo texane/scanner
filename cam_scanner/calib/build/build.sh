20aa766514c83feffc361e30f4a598f87adb961a
Fabien Le Mentec <texane@gmail.com>
2011-08-04 10:25:34 -0500

    [fix, add] fix cam_scanner/calib/conf

--- cam_scanner/calib/build/build.sh
+++ cam_scanner/calib/build/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
-g++ -Wall -O3 -I/usr/include/opencv \
+g++ \
+    -Wall -O3 \
+    -I/usr/include/opencv -I../src \
     ../src/main.cc \
     ../src/cvCalibrateProCam.cc \
     ../src/cvUtilProCam.cc \
