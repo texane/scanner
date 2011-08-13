1a32e60b0c4bd98be02fbd5e27216f05280f485a
Fabien Le Mentec <texane@gmail.com>
2011-08-13 09:47:14 -0500

    [update] get_shadow_points

--- cam_scanner/src/scan/src/main.cc
+++ cam_scanner/src/scan/src/main.cc
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <list>
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 #include "common/assert.hh"
@@ -28,108 +29,25 @@ static void show_matrix(CvMat* mat)
 }
 
 
-#if 0 // TODO
+// matrix helpers
 
-// intersection routines
-//
-// notes on representations
-// parametric equation: define a relation as a set of eq
-// implicit equation: f(x, y) = 0
-// explicit equation: f(x) = y
-// lines and planes are described by a coeff vector w.
-//
-// line parametric form:
-// p: q + u * v
-//
-// line explicit form
-// w[0] * x + w[1] = w[2]
-//
-// line implicit form
-// w[0] * x + w[1] * y + w[2] = 0
-//
-// plane explicit form:
-// w[0] * x + w[1] * y + w[2] * z = w[3]
-//
-// plane implicit form:
-// w[0] * x + w[1] * y + w[2] * z + w[3] = 0
-//
-
-
-// define vNT, a vector N sized vector of T typed elements.
-typedef int[2] v2i_t;
-typedef double[2] v2d_t;
-typedef double[3] v3d_t;
-typedef double[4] v4d_t;
-
-static int intersect_line_line
-(v2d_t* res, const v3d_t* la, const v3d_t* lb)
+static inline int get_gray_elem(const CvMat& m, unsigned int i, unsigned int j)
 {
-  // inertsect 2 co planar lines
-  // la and lb the lines explicit forms
-  // res the 2d resulting point
+  // get elem at i, j from a gray matrix (ie. uint8_t)
+  return CV_MAT_ELEM(m, unsigned char, i, j);
 }
 
-static int intersect_line_plane
-(vec3d_t* res, const vec3_t* q, const vect3_t* v, const vect3_t* w)
-{
-  // intersect a line with a plane
-  // res the resulting point in 3d coordinates
-  // q a point of the line
-  // v the line vector
-  // w a plane in implicit form
-}
-
-#endif // TODO
-
-
-#if 0 // fitting routines
-
-static void fit_line(void);
-static void fit_plane(void);
-
-#endif // fitting routines
-
-
-#if 0 // TODO
-
-static double norm(const double* v, unsigned int n)
-{
-  double sum = 0;
-  for (; n; --n, ++v) sum += (*v) * (*v);
-  return sqrt(sum);
-}
-
-static void normalize
-(double* v, const int* x, double fc, double cc, double kc, double alphac)
-{
-  // TOOLBOX_calib/normalize.m
-  // TODO
-}
-
-static void pixel_to_ray
-(double* v, const int* x, double fc, double cc, double kc, double alphac)
-{
-  // compute the camera coorrdinates of the ray starting
-  // at the camera point and passing by the pixel x.
-  //
-  // v the resulting ray vector
-  // x the pixel coordinates
-  // fc, cc, kc, alphac the camera intrinsic parameters
-
-  normalize(v, fc, cc, kc, alphac);
-
-  v[0] = x[0];
-  v[1] = x[1];
-  v[2] = 1;
-}
-
-#endif // TODO
 
 // video processing
 
 static inline int rewind_capture(CvCapture* cap)
 {
   return cvSetCaptureProperty(cap, CV_CAP_PROP_POS_FRAMES, 0);
+}
+
+static inline int seek_capture(CvCapture* cap, unsigned int n)
+{
+  return cvSetCaptureProperty(cap, CV_CAP_PROP_POS_FRAMES, n);
 }
 
 static inline unsigned int get_capture_frame_count(CvCapture* cap)
@@ -148,172 +66,6 @@ static CvSize get_capture_frame_size(CvCapture* cap)
   size = cvGetSize(frame);
   return size;
 }
-
-static int estimate_shadow_thresholds(CvCapture* cap, CvMat*& thresholds)
-{
-  // estimate invdiviual pixels shadow threshold
-  // algorithm:
-  // min_values = matrix(-inf);
-  // max_values = matrix(+inf);
-  // foreach frame
-  // . frame = rgb2gray(frame);
-  // . compute_minmax(frame, min_values, max_values);
-  // shadowValue = (min_values + max_values) / 2;
-
-  int error = -1;
-
-  IplImage* gray_image = NULL;
-  CvMat* gray_mat = NULL;
-
-  CvMat* minvals = NULL;
-  CvMat* maxvals = NULL;
-
-  const CvSize frame_size = get_capture_frame_size(cap);
-  const unsigned int nrows = frame_size.height;
-  const unsigned int ncols = frame_size.width;
-
-  // create and initialize min max matrces
-  // rgb2gray formula: 0.299 * R + 0.587 * G + 0.114 * B
-
-  minvals = cvCreateMat(nrows, ncols, CV_32SC1);
-  maxvals = cvCreateMat(nrows, ncols, CV_32SC1);
-  ASSERT_GOTO(minvals && maxvals, on_error);
-
-  for (unsigned int i = 0; i < nrows; ++i)
-    for (unsigned int j = 0; j < ncols; ++j)
-    {
-      CV_MAT_ELEM(*minvals, int, i, j) = 255;
-      CV_MAT_ELEM(*maxvals, int, i, j) = 0;
-    }
-
-  // foreach pixel of each frame, get minmax pixels
-
-  while (1)
-  {
-    IplImage* const frame_image = cvQueryFrame(cap);
-    if (frame_image == NULL) break ;
-
-    // create if not yet created
-    if (gray_image == NULL)
-    {
-      CvMat header;
-      gray_image = cvCreateImage(frame_size, IPL_DEPTH_8U, 1);
-      ASSERT_GOTO(gray_image, on_error);
-      gray_mat = cvGetMat(gray_image, &header);
-    }
-
-    // cvCopyImage(cam_frame, cam_frame_2);
-    cvCvtColor(frame_image, gray_image, CV_RGB2GRAY);
-
-    // get per pixel minmax
-    for (unsigned int i = 0; i < nrows; ++i)
-      for (unsigned int j = 0; j < ncols; ++j)
-      {
-	const int gray_val = CV_MAT_ELEM(*gray_mat, unsigned char, i, j);
-
-	if (CV_MAT_ELEM(*minvals, int, i, j) > gray_val)
-	  CV_MAT_ELEM(*minvals, int, i, j) = gray_val;
-
-	if (CV_MAT_ELEM(*maxvals, int, i, j) < gray_val)
-	  CV_MAT_ELEM(*maxvals, int, i, j) = gray_val;
-      }
-
-    // dont release frame_image
-  }
-
-  // create and get threshold
-
-  thresholds = cvCreateMat(nrows, ncols, CV_8UC1);
-  ASSERT_GOTO(thresholds, on_error);
-
-  for (unsigned int i = 0; i < nrows; ++i)
-    for (unsigned int j = 0; j < ncols; ++j)
-    {
-      const int minval = CV_MAT_ELEM(*minvals, int, i, j);
-      const int maxval = CV_MAT_ELEM(*maxvals, int, i, j);
-      CV_MAT_ELEM(*thresholds, unsigned char, i, j) = (minval + maxval) / 2;
-    }
-
-  // success
-  error = 0;
-
- on_error:
-  if (minvals) cvReleaseMat(&minvals);
-  if (maxvals) cvReleaseMat(&maxvals);
-  if (gray_image) cvReleaseImage(&gray_image);
-
-  if ((error == -1) && thresholds)
-    cvReleaseMat(&thresholds);
-
-  rewind_capture(cap);
-
-  return error;
-}
-
-#if 0 // TODO
-static int estimate_shadow_planes(CvCapture* cap)
-{
-  // estimate shadow plane parameters
-  // algorithm:
-  // foreach_frame:
-  // . frame = rgb2gray(frame);
-  // . estimate vline
-  // . estimate hline
-
-  rewind_capture(cap);
-
-  return -1;
-}
-#endif // TODO
-
-#if 0 // TODO
-static int estimate_shadow_cross_times(CvCapture* cap)
-{
-  // estimate the per pixel shadow crossing time
-  // where time is the frame index. a pixel is
-  // considered entered by a shadow when its gray
-  // intensity changes from non shadow to shadow
-
-  rewind_capture(cap);
-
-  return -1;
-}
-#endif // TODO
-
-
-#if 0 // TODO
-static void show_estimations(CvCapture* cap)
-{
-}
-#endif // TODO
-
-
-#if 0 // TODO
-static void generate_depth_map(void)
-{
-  // foreach shadowed pixel
-  // . generate ray equation with camera pos
-  // . intersect with shadow plane
-}
-#endif // TODO
-
-
-#if 0 // TODO
-static void show_depth_map(CvMat* depth_map)
-{
-  // depth map a double matrix
-}
-#endif // TODO
-
-
-#if 0 // TODO
-static void generate_points
-(scan_data_t& scan_data, std::list<v3d_t>& scan_points)
-{
-  // foreach pixel, generate the parametric ray equation
-  // and inersect with the corresponding shadow plane
-}
-#endif // TODO
 
 
 // ask user for a set of points
@@ -465,6 +217,8 @@ static int get_user_points(CvCapture* cap, user_points_t& points)
   int error = -1;
   on_mouse_state_t state;
 
+  rewind_capture(cap);
+
   frame = cvQueryFrame(cap);
   ASSERT_GOTO(frame, on_error);
   cloned_image = cvCloneImage(frame);
@@ -493,10 +247,393 @@ static int get_user_points(CvCapture* cap, user_points_t& points)
  on_error:
   if (cloned_image) cvReleaseImage(&cloned_image);
   cvDestroyWindow(name);
-  rewind_capture(cap);
 
   return error;
 }
+
+
+#if 0 // TODO
+
+// intersection routines
+//
+// notes on representations
+// parametric equation: define a relation as a set of eq
+// implicit equation: f(x, y) = 0
+// explicit equation: f(x) = y
+// lines and planes are described by a coeff vector w.
+//
+// line parametric form:
+// p: q + u * v
+//
+// line explicit form
+// w[0] * x + w[1] = w[2]
+//
+// line implicit form
+// w[0] * x + w[1] * y + w[2] = 0
+//
+// plane explicit form:
+// w[0] * x + w[1] * y + w[2] * z = w[3]
+//
+// plane implicit form:
+// w[0] * x + w[1] * y + w[2] * z + w[3] = 0
+//
+
+
+// define vNT, a vector N sized vector of T typed elements.
+typedef int[2] v2i_t;
+typedef double[2] v2d_t;
+typedef double[3] v3d_t;
+typedef double[4] v4d_t;
+
+static int intersect_line_line
+(v2d_t* res, const v3d_t* la, const v3d_t* lb)
+{
+  // inertsect 2 co planar lines
+  // la and lb the lines explicit forms
+  // res the 2d resulting point
+}
+
+static int intersect_line_plane
+(vec3d_t* res, const vec3_t* q, const vect3_t* v, const vect3_t* w)
+{
+  // intersect a line with a plane
+  // res the resulting point in 3d coordinates
+  // q a point of the line
+  // v the line vector
+  // w a plane in implicit form
+}
+
+#endif // TODO
+
+
+#if 0 // fitting routines
+
+static void fit_line(void);
+static void fit_plane(void);
+
+#endif // fitting routines
+
+
+#if 0 // TODO
+
+static double norm(const double* v, unsigned int n)
+{
+  double sum = 0;
+  for (; n; --n, ++v) sum += (*v) * (*v);
+  return sqrt(sum);
+}
+
+static void normalize
+(double* v, const int* x, double fc, double cc, double kc, double alphac)
+{
+  // TOOLBOX_calib/normalize.m
+  // TODO
+}
+
+static void pixel_to_ray
+(double* v, const int* x, double fc, double cc, double kc, double alphac)
+{
+  // compute the camera coorrdinates of the ray starting
+  // at the camera point and passing by the pixel x.
+  //
+  // v the resulting ray vector
+  // x the pixel coordinates
+  // fc, cc, kc, alphac the camera intrinsic parameters
+
+  normalize(v, fc, cc, kc, alphac);
+
+  v[0] = x[0];
+  v[1] = x[1];
+  v[2] = 1;
+}
+
+#endif // TODO
+
+
+// shadow threshold estimtation
+
+static int estimate_shadow_thresholds(CvCapture* cap, CvMat*& thresholds)
+{
+  // estimate invdiviual pixels shadow threshold
+  // algorithm:
+  // min_values = matrix(-inf);
+  // max_values = matrix(+inf);
+  // foreach frame
+  // . frame = rgb2gray(frame);
+  // . compute_minmax(frame, min_values, max_values);
+  // shadowValue = (min_values + max_values) / 2;
+
+  int error = -1;
+
+  IplImage* gray_image = NULL;
+  CvMat* gray_mat = NULL;
+
+  CvMat* minvals = NULL;
+  CvMat* maxvals = NULL;
+
+  // fixme
+  rewind_capture(cap);
+  const CvSize frame_size = get_capture_frame_size(cap);
+  const unsigned int nrows = frame_size.height;
+  const unsigned int ncols = frame_size.width;
+
+  rewind_capture(cap);
+
+  // create and initialize min max matrces
+  // rgb2gray formula: 0.299 * R + 0.587 * G + 0.114 * B
+
+  minvals = cvCreateMat(nrows, ncols, CV_32SC1);
+  maxvals = cvCreateMat(nrows, ncols, CV_32SC1);
+  ASSERT_GOTO(minvals && maxvals, on_error);
+
+  for (unsigned int i = 0; i < nrows; ++i)
+    for (unsigned int j = 0; j < ncols; ++j)
+    {
+      CV_MAT_ELEM(*minvals, int, i, j) = 255;
+      CV_MAT_ELEM(*maxvals, int, i, j) = 0;
+    }
+
+  // foreach pixel of each frame, get minmax pixels
+
+  while (1)
+  {
+    IplImage* const frame_image = cvQueryFrame(cap);
+    if (frame_image == NULL) break ;
+
+    // create if does not yet exist
+    if (gray_image == NULL)
+    {
+      CvMat header;
+      gray_image = cvCreateImage(frame_size, IPL_DEPTH_8U, 1);
+      ASSERT_GOTO(gray_image, on_error);
+      gray_mat = cvGetMat(gray_image, &header);
+    }
+
+    cvCvtColor(frame_image, gray_image, CV_RGB2GRAY);
+
+    // get per pixel minmax
+    for (unsigned int i = 0; i < nrows; ++i)
+      for (unsigned int j = 0; j < ncols; ++j)
+      {
+	const int gray_val = CV_MAT_ELEM(*gray_mat, unsigned char, i, j);
+
+	if (CV_MAT_ELEM(*minvals, int, i, j) > gray_val)
+	  CV_MAT_ELEM(*minvals, int, i, j) = gray_val;
+
+	if (CV_MAT_ELEM(*maxvals, int, i, j) < gray_val)
+	  CV_MAT_ELEM(*maxvals, int, i, j) = gray_val;
+      }
+
+    // dont release frame_image
+  }
+
+  // create and get threshold
+
+  thresholds = cvCreateMat(nrows, ncols, CV_8UC1);
+  ASSERT_GOTO(thresholds, on_error);
+
+  for (unsigned int i = 0; i < nrows; ++i)
+    for (unsigned int j = 0; j < ncols; ++j)
+    {
+      const int minval = CV_MAT_ELEM(*minvals, int, i, j);
+      const int maxval = CV_MAT_ELEM(*maxvals, int, i, j);
+      CV_MAT_ELEM(*thresholds, unsigned char, i, j) = (minval + maxval) / 2;
+    }
+
+  // success
+  error = 0;
+
+ on_error:
+  if (minvals) cvReleaseMat(&minvals);
+  if (maxvals) cvReleaseMat(&maxvals);
+  if (gray_image) cvReleaseImage(&gray_image);
+
+  if ((error == -1) && thresholds)
+    cvReleaseMat(&thresholds);
+
+  return error;
+}
+
+__attribute__((unused)) static void draw_points
+(IplImage* image, const std::list<CvPoint>& points, const CvScalar& color)
+{
+  std::list<CvPoint>::const_iterator pos = points.begin();
+  std::list<CvPoint>::const_iterator end = points.end();
+  for (; pos != end; ++pos) cvCircle(image, *pos, 1, color, -1);
+}
+
+static void get_shadow_points
+(
+ const CvMat* gray_mat,
+ const CvMat* thr_mat,
+ const CvRect& bbox,
+ std::list<CvPoint> points[2]
+)
+{
+
+  // get the entering and leaving shaded points (resp. points[0] and points[1])
+  // in the bounding box defined by {first,last}_{row,col}. see comments for the
+  // definition of entering and leaving points.
+
+  for (int i = bbox.y; i < bbox.y + bbox.height; ++i)
+    for (int j = bbox.x; j < bbox.x + bbox.width; ++j)
+    {
+      const int thr_val = get_gray_elem(*thr_mat, i, j);
+      const int gray_val = get_gray_elem(*gray_mat, i, j);
+      if (gray_val >= thr_val) continue ;
+
+      const int lthr_val = get_gray_elem(*thr_mat, i, j - 1);
+      const int rthr_val = get_gray_elem(*thr_mat, i, j + 1);
+      const int lneigh_val = get_gray_elem(*gray_mat, i, j - 1);
+      const int rneigh_val = get_gray_elem(*gray_mat, i, j + 1);
+
+      CvPoint point;
+      point.x = j;
+      point.y = i;
+
+      // current shaded, left not shaded: entering pixel
+      if (lneigh_val >= lthr_val) points[0].push_back(point);
+
+      // current shaded, right not shaded: leaving pixel
+      if (rneigh_val >= rthr_val) points[1].push_back(point);
+    }
+}
+
+static int estimate_shadow_planes
+(CvCapture* cap, const CvMat* thr_mat, const user_points_t& user_points)
+{
+  // estimate shadow plane parameters
+  // foreach_frame, estimate {h,v}line
+
+  IplImage* gray_image = NULL;
+  CvMat* gray_mat = NULL;
+
+  // {vertical,horizontal}_{enter,leave}
+  std::list<CvPoint> shadow_points[4];
+
+  int error = -1;
+
+  // compute bounding boxes according to user inputs
+  CvRect vbox;
+  vbox.x = user_points.vplane[0].x + 1;
+  vbox.width = (user_points.vplane[1].x - 1) - vbox.x;
+  vbox.y = user_points.vplane[0].y + 1;
+  vbox.height = (user_points.vplane[1].y - 1) - vbox.y;
+
+  CvRect hbox;
+  hbox.x = user_points.hplane[0].x + 1;
+  hbox.width = (user_points.hplane[1].x - 1) - hbox.x;
+  hbox.y = user_points.hplane[0].y + 1;
+  hbox.height = (user_points.hplane[1].y - 1) - hbox.y;
+
+  // toremove
+  seek_capture(cap, 60);
+  // rewind_capture(cap);
+
+  while (1)
+  {
+    IplImage* const frame_image = cvQueryFrame(cap);
+    if (frame_image == NULL) break ;
+
+    // create if does not yet exist
+    if (gray_image == NULL)
+    {
+      CvMat header;
+      gray_image = cvCreateImage(cvGetSize(frame_image), IPL_DEPTH_8U, 1);
+      ASSERT_GOTO(gray_image, on_error);
+      gray_mat = cvGetMat(gray_image, &header);
+    }
+
+    cvCvtColor(frame_image, gray_image, CV_RGB2GRAY);
+
+    // find pixels whose value is below the shadow thresold. if the
+    // same row left (resp. right) adjacent pixel is not a shadowed
+    // one, the the pixel belongs to the entering (resp. leaving)
+    // shadow line.
+    // limit the search to the bounding boxes indicated by the user
+    // input.
+
+    for (unsigned int i = 0; i < 4; ++i) shadow_points[i].clear();
+    get_shadow_points(gray_mat, thr_mat, vbox, shadow_points + 0);
+    get_shadow_points(gray_mat, thr_mat, hbox, shadow_points + 2);
+
+#if 1 // draw the points
+    {
+      static const CvScalar colors[4] =
+      {
+	CV_RGB(0xff, 0x00, 0x00),
+	CV_RGB(0x00, 0xff, 0x00),
+	CV_RGB(0x00, 0x00, 0xff),
+	CV_RGB(0xff, 0x00, 0xff)
+      };
+
+      IplImage* cloned_image = cvCloneImage(frame_image);
+      ASSERT_GOTO(cloned_image, on_error);
+
+      for (unsigned int i = 0; i < 4; ++i)
+	draw_points(cloned_image, shadow_points[i], colors[i]);
+
+      show_image(cloned_image, "ShadowPoints");
+      cvReleaseImage(&cloned_image);
+    }
+#endif // draw the points
+  }
+
+  error = 0;
+
+ on_error:
+  if (gray_image) cvReleaseImage(&gray_image);
+  return error;
+}
+
+#if 0 // TODO
+static int estimate_shadow_cross_times(CvCapture* cap)
+{
+  // estimate the per pixel shadow crossing time
+  // where time is the frame index. a pixel is
+  // considered entered by a shadow when its gray
+  // intensity changes from non shadow to shadow
+
+  rewind_capture(cap);
+
+  return -1;
+}
+#endif // TODO
+
+
+#if 0 // TODO
+static void show_estimations(CvCapture* cap)
+{
+}
+#endif // TODO
+
+
+#if 0 // TODO
+static void generate_depth_map(void)
+{
+  // foreach shadowed pixel
+  // . generate ray equation with camera pos
+  // . intersect with shadow plane
+}
+#endif // TODO
+
+
+#if 0 // TODO
+static void show_depth_map(CvMat* depth_map)
+{
+  // depth map a double matrix
+}
+#endif // TODO
+
+
+#if 0 // TODO
+static void generate_points
+(scan_data_t& scan_data, std::list<v3d_t>& scan_points)
+{
+  // foreach pixel, generate the parametric ray equation
+  // and inersect with the corresponding shadow plane
+}
+#endif // TODO
 
 
 // scan main routine
@@ -504,12 +641,8 @@ static int get_user_points(CvCapture* cap, user_points_t& points)
 static int do_scan(CvCapture* cap, const cam_params_t& params)
 {
   CvMat* shadow_thresholds = NULL;
-  CvSize frame_size;
   user_points_t user_points;
   int error = -1;
-
-  frame_size = get_capture_frame_size(cap);
-  ASSERT_GOTO(frame_size.width, on_error);
 
   error = estimate_shadow_thresholds(cap, shadow_thresholds);
   ASSERT_GOTO(error == 0, on_error);
@@ -522,9 +655,11 @@ static int do_scan(CvCapture* cap, const cam_params_t& params)
 
   // print_user_points(user_points);
 
-#if 0
-  error = estimate_shadow_planes(cap, shadow_planes);
+  error = estimate_shadow_planes
+    (cap, shadow_thresholds, user_points);
   ASSERT_GOTO(error == 0, on_error);
+
+#if 0
 
   error = estimate_shadow_cross_times(cap, shadow_xtimes);
   ASSERT_GOTO(error == 0, on_error);
